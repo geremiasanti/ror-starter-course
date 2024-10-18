308beb2dc2b98306497e80eb091f3b54c3e2794c
geremia <germysettesette@gmail.com>
2024-10-18 12:18:23 +0200

    var referencing

--- ruby/strings.rb
+++ ruby/strings.rb
@@ -8,3 +8,9 @@ puts 'Il mio nome completo e' #{full_name}!'
 
 puts full_name.class
 puts full_name.methods.join(', ')
+
+first_var = "hei"
+first_var_assigned_to_this = first_var
+first_var = "heyyyy"
+puts "first_var: #{first_var}\nfirst_var_assigned_to_this: #{first_var_assigned_to_this}"
+
