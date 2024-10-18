77d8be9608c304c318efe412f6b7ba8013648708
geremia <germysettesette@gmail.com>
2024-10-18 12:14:04 +0200

    get var class and methods

--- ruby/strings.rb
+++ ruby/strings.rb
@@ -1,6 +1,10 @@
 first_name = "Geremia"
 last_name = "Santi"
 full_name = first_name + " " + last_name
+
 puts "Il mio nome completo e' \#{full_name}!"
 puts "Il mio nome completo e' #{full_name}!"
 puts 'Il mio nome completo e' #{full_name}!'
+
+puts full_name.class
+puts full_name.methods.join(', ')
